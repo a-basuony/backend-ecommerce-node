1afc3c083638b06dac6a076218b7d15f86b3f14c
Ahmed Basuony <A.Basuony.social@gmail.com>
2026-02-07 02:55:03 +0200

    upload

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
 import serverless from "serverless-http";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
@@ -19,6 +20,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL || "http://localhost:5173", // Replace with your frontend URL
+  credentials: true, // Allow cookies to be sent
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
